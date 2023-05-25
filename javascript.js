918af69cbc3162f265287c65ab9e85e89e626036
deepikakgowda <deepikakgowda37@gmail.com>
2023-05-25 11:38:50 +0530

    New files added and update index.html file

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function myFunc() {
+    var fid = document.getElementById("firstName").value;
+    var lid = document.getElementById("lastName").value;
+    console.log(fid);
+    console.log(lid);
+    alert(fid + " " + lid);
+}
